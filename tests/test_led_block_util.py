611fb0d7f5e4f8e2b8752ae4f10fe882cfe080c9
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2017-09-26 23:12:18 +0900

    20170926

--- tests/test_led_block_util.py
+++ tests/test_led_block_util.py
@@ -5,20 +5,82 @@ import json
 class TestLedBlockUtil(unittest.TestCase):
 
     # get_blocks_in_loop
+    test_data = """
+    {
+        "orders":[
+            {
+                "id":"filter-2",
+                "lifetime":1,
+                "param":{
+                    "offset":1
+                }
+            },
+            {
+                "id":"object-2",
+                "lifetime":1,
+                "param":{
+                    "offset":1
+                }
+            },
+            {
+                "id":"object-1",
+                "lifetime":1,
+                "param":{
+                    "offset":1
+                }
+            },
+            {
+                "id":"object-2",
+                "lifetime":1,
+                "param":{
+                    "offset":1
+                }
+            },
+            {
+                "id":"object-1",
+                "lifetime":1,
+                "param":{
+                    "offset":1
+                }
+            },
+            {
+                "id":"filter-0",
+                "lifetime":1,
+                "param":{
+                    "offset":1
+                }
+            },
+            {
+                "id":"object-1",
+                "lifetime":1,
+                "param":{
+                    "offset":1
+                }
+            },
+            {
+                "id":"object-2",
+                "lifetime":1,
+                "param":{
+                    "offset":1
+                }
+            }
+        ]
+    }
+    """
 
     def test_get_blocks_in_loop_1(self):
 
-        orders = ["test","ctrl-1","test2","ctrl-1","test"]
+        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
 
         actual = get_blocks_in_loop(orders, 0)
 
         self.assertEqual(
-            ["test"],
+            [{"id":"test"}],
             actual)
 
     def test_get_blocks_in_loop_2(self):
 
-        orders = ["test","ctrl-1","test2","ctrl-1","test"]
+        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
 
         actual = get_blocks_in_loop(orders, 1)
 
@@ -28,27 +90,27 @@ class TestLedBlockUtil(unittest.TestCase):
 
     def test_get_blocks_in_loop_3(self):
 
-        orders = ["test","ctrl-1","test2","ctrl-1","test"]
+        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
 
         actual = get_blocks_in_loop(orders, 2)
 
         self.assertEqual(
-            ["test2"],
+            [{"id":"test2"}],
             actual)
 
     def test_get_blocks_in_loop_4(self):
 
-        orders = ["test","ctrl-1","test2","ctrl-1","test"]
+        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
 
         actual = get_blocks_in_loop(orders, 4)
 
         self.assertEqual(
-            ["test"],
+            [{"id":"test"}],
             actual)
 
     def test_get_blocks_in_loop_5(self):
 
-        orders = ["test","ctrl-1","ctrl-1","test"]
+        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"test"}]
 
         actual = get_blocks_in_loop(orders, 1)
 
@@ -59,49 +121,49 @@ class TestLedBlockUtil(unittest.TestCase):
     
     def test_flatten_blocks_1(self):
 
-        orders = ["test","ctrl-1","test2","ctrl-1","test"]
+        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
 
         actual = flatten_blocks(orders)
 
         self.assertEqual(
-            ["test", "test2", "test2", "test2", "test"],
+            [{"id":"test"}, {"id":"test2"}, {"id":"test2"}, {"id":"test2"}, {"id":"test"}],
             actual)
 
     def test_flatten_blocks_2(self):
 
-        orders = ["test","ctrl-1","test2","test2","test"]
+        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"test2"},{"id":"test"}]
 
         actual = flatten_blocks(orders)
 
         self.assertEqual(
-            ["test","test2","test2","test","test2","test2","test","test2","test2","test"],
+            [{"id":"test"},{"id":"test2"},{"id":"test2"},{"id":"test"},{"id":"test2"},{"id":"test2"},{"id":"test"},{"id":"test2"},{"id":"test2"},{"id":"test"}],
             actual)
 
     def test_flatten_blocks_3(self):
 
-        orders = ["test","ctrl-1","ctrl-1","test2","test"]
+        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"test2"},{"id":"test"}]
 
         actual = flatten_blocks(orders)
 
         self.assertEqual(
-            ["test","test2","test"],
+            [{"id":"test"},{"id":"test2"},{"id":"test"}],
             actual)
 
 
     def test_flatten_blocks_4(self):
 
-        orders = ["test","test2","test","test","ctrl-1"]
+        orders = [{"id":"test"},{"id":"test2"},{"id":"test"},{"id":"test"},{"id":"ctrl-1"}]
 
         actual = flatten_blocks(orders)
 
         self.assertEqual(
-            ["test","test2","test","test"],
+            [{"id":"test"},{"id":"test2"},{"id":"test"},{"id":"test"}],
             actual)
 
 
     def test_flatten_blocks_5(self):
 
-        orders = ["ctrl-1"]
+        orders = [{"id":"ctrl-1"}]
 
         actual = flatten_blocks(orders)
 
@@ -111,7 +173,7 @@ class TestLedBlockUtil(unittest.TestCase):
 
     def test_flatten_blocks_6(self):
 
-        orders = ["ctrl-1","ctrl-1","ctrl-1","ctrl-1","ctrl-1","ctrl-1"]
+        orders = [{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"ctrl-1"}]
 
         actual = flatten_blocks(orders)
 
@@ -121,22 +183,22 @@ class TestLedBlockUtil(unittest.TestCase):
 
     def test_flatten_blocks_7(self):
 
-        orders = ["ctrl-1","test"]
+        orders = [{"id":"ctrl-1"},{"id":"test"}]
 
         actual = flatten_blocks(orders)
 
         self.assertEqual(
-            ["test", "test", "test"],
+            [{"id":"test"}, {"id":"test"}, {"id":"test"}],
             actual)
-
+            
 
     def test_create_block_1(self):
-        actual = create_block('object-1', None)
+        actual = create_block({"id":"object-1"}, None)
         self.assertTrue(isinstance(actual, LedFillObject))
 
 
     def test_create_block_1(self):
-        actual = create_block('filter-1', None)
+        actual = create_block({"id":"filter-1"}, None)
         self.assertTrue(isinstance(actual, LedHsvCanvasFilter))
 
 
@@ -153,70 +215,16 @@ class TestLedBlockUtil(unittest.TestCase):
 
 
     def test_json_loads3(self):
- #test_data1 = {"filter-2","object-2","object-1","object-2","object-1","filter-0","object-2","object-1"} # this line will be json
-        data = """
-        {
-            "orders":[
-                {
-                    "id":"filter-2",
-                    "lifetime":1,
-                    "param":{
-                        "offset":1
-                    }
-                },
-                {
-                    "id":"object-2",
-                    "lifetime":1,
-                    "param":{
-                        "offset":1
-                    }
-                },
-                {
-                    "id":"object-1",
-                    "lifetime":1,
-                    "param":{
-                        "offset":1
-                    }
-                },
-                {
-                    "id":"object-2",
-                    "lifetime":1,
-                    "param":{
-                        "offset":1
-                    }
-                },
-                {
-                    "id":"object-1",
-                    "lifetime":1,
-                    "param":{
-                        "offset":1
-                    }
-                },
-                {
-                    "id":"filter-0",
-                    "lifetime":1,
-                    "param":{
-                        "offset":1
-                    }
-                },
-                {
-                    "id":"object-1",
-                    "lifetime":1,
-                    "param":{
-                        "offset":1
-                    }
-                },
-                {
-                    "id":"object-2",
-                    "lifetime":1,
-                    "param":{
-                        "offset":1
-                    }
-                }
-            ]
-        }
-        """
 
-        actual = json.loads(data)
+        actual = json.loads(TestLedBlockUtil.test_data)
         orders = actual['orders']
         self.assertEqual(8, len(orders))
+
+    def test_get_lifetime_from_block_1(self):
+        data = {"abc":"def", "lifetime":1}
+        self.assertEqual(1, get_lifetime_from_block(data))
+
+    def test_get_lifetime_from_block_2(self):
+        data = {"abc":"def"}
+        self.assertEqual(5, get_lifetime_from_block(data))
+
