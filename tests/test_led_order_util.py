ba418c52cd3ed8fa992d85ebc787ef8c862b88d9
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2017-09-27 22:35:11 +0900

    fix test fail

--- tests/test_led_order_util.py
+++ tests/test_led_order_util.py
@@ -70,7 +70,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_get_orders_in_loop_1(self):
 
-        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
+        orders = [{"id":"test"},{"id":"ctrl-loop"},{"id":"test2"},{"id":"ctrl-loop"},{"id":"test"}]
 
         actual = get_orders_in_loop(orders, 0)
 
@@ -80,7 +80,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_get_orders_in_loop_2(self):
 
-        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
+        orders = [{"id":"test"},{"id":"ctrl-loop"},{"id":"test2"},{"id":"ctrl-loop"},{"id":"test"}]
 
         actual = get_orders_in_loop(orders, 1)
 
@@ -90,7 +90,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_get_orders_in_loop_3(self):
 
-        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
+        orders = [{"id":"test"},{"id":"ctrl-loop"},{"id":"test2"},{"id":"ctrl-loop"},{"id":"test"}]
 
         actual = get_orders_in_loop(orders, 2)
 
@@ -100,7 +100,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_get_orders_in_loop_4(self):
 
-        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
+        orders = [{"id":"test"},{"id":"ctrl-loop"},{"id":"test2"},{"id":"ctrl-loop"},{"id":"test"}]
 
         actual = get_orders_in_loop(orders, 4)
 
@@ -110,7 +110,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_get_orders_in_loop_5(self):
 
-        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"test"}]
+        orders = [{"id":"test"},{"id":"ctrl-loop"},{"id":"ctrl-loop"},{"id":"test"}]
 
         actual = get_orders_in_loop(orders, 1)
 
@@ -121,7 +121,7 @@ class TestLedOrderUtil(unittest.TestCase):
     
     def test_flatten_orders_1(self):
 
-        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"ctrl-1"},{"id":"test"}]
+        orders = [{"id":"test"},{"id":"ctrl-loop"},{"id":"test2"},{"id":"ctrl-loop"},{"id":"test"}]
 
         actual = flatten_orders(orders)
 
@@ -131,7 +131,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_flatten_orders_2(self):
 
-        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"test2"},{"id":"test2"},{"id":"test"}]
+        orders = [{"id":"test"},{"id":"ctrl-loop"},{"id":"test2"},{"id":"test2"},{"id":"test"}]
 
         actual = flatten_orders(orders)
 
@@ -141,7 +141,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_flatten_orders_3(self):
 
-        orders = [{"id":"test"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"test2"},{"id":"test"}]
+        orders = [{"id":"test"},{"id":"ctrl-loop"},{"id":"ctrl-loop"},{"id":"test2"},{"id":"test"}]
 
         actual = flatten_orders(orders)
 
@@ -152,7 +152,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_flatten_orders_4(self):
 
-        orders = [{"id":"test"},{"id":"test2"},{"id":"test"},{"id":"test"},{"id":"ctrl-1"}]
+        orders = [{"id":"test"},{"id":"test2"},{"id":"test"},{"id":"test"},{"id":"ctrl-loop"}]
 
         actual = flatten_orders(orders)
 
@@ -163,7 +163,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_flatten_orders_5(self):
 
-        orders = [{"id":"ctrl-1"}]
+        orders = [{"id":"ctrl-loop"}]
 
         actual = flatten_orders(orders)
 
@@ -173,7 +173,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_flatten_orders_6(self):
 
-        orders = [{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"ctrl-1"},{"id":"ctrl-1"}]
+        orders = [{"id":"ctrl-loop"},{"id":"ctrl-loop"},{"id":"ctrl-loop"},{"id":"ctrl-loop"},{"id":"ctrl-loop"},{"id":"ctrl-loop"}]
 
         actual = flatten_orders(orders)
 
@@ -183,7 +183,7 @@ class TestLedOrderUtil(unittest.TestCase):
 
     def test_flatten_orders_7(self):
 
-        orders = [{"id":"ctrl-1"},{"id":"test"}]
+        orders = [{"id":"ctrl-loop"},{"id":"test"}]
 
         actual = flatten_orders(orders)
 
@@ -193,17 +193,17 @@ class TestLedOrderUtil(unittest.TestCase):
             
 
     def test_create_order_1(self):
-        actual = create_order({"id":"object-1"}, None)
+        actual = create_order({"id":"object-fill"}, None)
         self.assertTrue(isinstance(actual, LedFillObject))
 
 
     def test_create_order_2(self):
-        actual = create_order({"id":"filter-1"}, None)
+        actual = create_order({"id":"filter-hsv"}, None)
         self.assertTrue(isinstance(actual, LedHsvCanvasFilter))
 
 
     def test_create_order_3(self):
-        actual = create_order({"id":"object-bitmap", "param":{"bitmap":"iVBORw0KGgoAAAANSUhEUgAAABAAAAAgCAYAAAAbifjMAAAEDWlDQ1BJQ0MgUHJvZmlsZQAAOI2NVV1oHFUUPrtzZyMkzlNsNIV0qD8NJQ2TVjShtLp/3d02bpZJNtoi6GT27s6Yyc44M7v9oU9FUHwx6psUxL+3gCAo9Q/bPrQvlQol2tQgKD60+INQ6Ium65k7M5lpurHeZe58853vnnvuuWfvBei5qliWkRQBFpquLRcy4nOHj4g9K5CEh6AXBqFXUR0rXalMAjZPC3e1W99Dwntf2dXd/p+tt0YdFSBxH2Kz5qgLiI8B8KdVy3YBevqRHz/qWh72Yui3MUDEL3q44WPXw3M+fo1pZuQs4tOIBVVTaoiXEI/MxfhGDPsxsNZfoE1q66ro5aJim3XdoLFw72H+n23BaIXzbcOnz5mfPoTvYVz7KzUl5+FRxEuqkp9G/Ajia219thzg25abkRE/BpDc3pqvphHvRFys2weqvp+krbWKIX7nhDbzLOItiM8358pTwdirqpPFnMF2xLc1WvLyOwTAibpbmvHHcvttU57y5+XqNZrLe3lE/Pq8eUj2fXKfOe3pfOjzhJYtB/yll5SDFcSDiH+hRkH25+L+sdxKEAMZahrlSX8ukqMOWy/jXW2m6M9LDBc31B9LFuv6gVKg/0Szi3KAr1kGq1GMjU/aLbnq6/lRxc4XfJ98hTargX++DbMJBSiYMIe9Ck1YAxFkKEAG3xbYaKmDDgYyFK0UGYpfoWYXG+fAPPI6tJnNwb7ClP7IyF+D+bjOtCpkhz6CFrIa/I6sFtNl8auFXGMTP34sNwI/JhkgEtmDz14ySfaRcTIBInmKPE32kxyyE2Tv+thKbEVePDfW/byMM1Kmm0XdObS7oGD/MypMXFPXrCwOtoYjyyn7BV29/MZfsVzpLDdRtuIZnbpXzvlf+ev8MvYr/Gqk4H/kV/G3csdazLuyTMPsbFhzd1UabQbjFvDRmcWJxR3zcfHkVw9GfpbJmeev9F08WW8uDkaslwX6avlWGU6NRKz0g/SHtCy9J30o/ca9zX3Kfc19zn3BXQKRO8ud477hLnAfc1/G9mrzGlrfexZ5GLdn6ZZrrEohI2wVHhZywjbhUWEy8icMCGNCUdiBlq3r+xafL549HQ5jH+an+1y+LlYBifuxAvRN/lVVVOlwlCkdVm9NOL5BE4wkQ2SMlDZU97hX86EilU/lUmkQUztTE6mx1EEPh7OmdqBtAvv8HdWpbrJS6tJj3n0CWdM6busNzRV3S9KTYhqvNiqWmuroiKgYhshMjmhTh9ptWhsF7970j/SbMrsPE1suR5z7DMC+P/Hs+y7ijrQAlhyAgccjbhjPygfeBTjzhNqy28EdkUh8C+DU9+z2v/oyeH791OncxHOs5y2AtTc7nb/f73TWPkD/qwBnjX8BoJ98VVBg/m8AAAEVSURBVEgN7VXBDcIwDHQqRmKATsME/Gn/TMA0HYCdis/tpY6VVqC8QFhynNi5i7kmIs1q0mBdA9agzQQndpBS4vSjCNSXa9As4p9A5Ac0yG+BD6B2r49eSfEWamASM0ayQsSxF6HL8yJwrBEBjGBNbV9h6EVudwWpMdpiHWYlgUcrOmBxvD44NTK/jiRZA3QAi6d7MOrDhHGzTMBUPIF5xHTeOmO+IMhdTIuY3BRPZR4x3wOCfRHz26RDr75aJOuQr4EBNPAKZKjtnfV3m2sR/zLLGtHNrbbuUzLcOXPToBCuIpSd7u6AF7Njaxb3wCju1IqvgJ5oKMBquaWyjJnAixOVPqrZT/AbPHttHvfmDmqb38m9APu7aLyM3S4UAAAAAElFTkSuQmCC"}}, None)
+        actual = create_order({"id":"object-bitmap", "param":{"z":1, "bitmap":"iVBORw0KGgoAAAANSUhEUgAAABAAAAAgCAYAAAAbifjMAAAEDWlDQ1BJQ0MgUHJvZmlsZQAAOI2NVV1oHFUUPrtzZyMkzlNsNIV0qD8NJQ2TVjShtLp/3d02bpZJNtoi6GT27s6Yyc44M7v9oU9FUHwx6psUxL+3gCAo9Q/bPrQvlQol2tQgKD60+INQ6Ium65k7M5lpurHeZe58853vnnvuuWfvBei5qliWkRQBFpquLRcy4nOHj4g9K5CEh6AXBqFXUR0rXalMAjZPC3e1W99Dwntf2dXd/p+tt0YdFSBxH2Kz5qgLiI8B8KdVy3YBevqRHz/qWh72Yui3MUDEL3q44WPXw3M+fo1pZuQs4tOIBVVTaoiXEI/MxfhGDPsxsNZfoE1q66ro5aJim3XdoLFw72H+n23BaIXzbcOnz5mfPoTvYVz7KzUl5+FRxEuqkp9G/Ajia219thzg25abkRE/BpDc3pqvphHvRFys2weqvp+krbWKIX7nhDbzLOItiM8358pTwdirqpPFnMF2xLc1WvLyOwTAibpbmvHHcvttU57y5+XqNZrLe3lE/Pq8eUj2fXKfOe3pfOjzhJYtB/yll5SDFcSDiH+hRkH25+L+sdxKEAMZahrlSX8ukqMOWy/jXW2m6M9LDBc31B9LFuv6gVKg/0Szi3KAr1kGq1GMjU/aLbnq6/lRxc4XfJ98hTargX++DbMJBSiYMIe9Ck1YAxFkKEAG3xbYaKmDDgYyFK0UGYpfoWYXG+fAPPI6tJnNwb7ClP7IyF+D+bjOtCpkhz6CFrIa/I6sFtNl8auFXGMTP34sNwI/JhkgEtmDz14ySfaRcTIBInmKPE32kxyyE2Tv+thKbEVePDfW/byMM1Kmm0XdObS7oGD/MypMXFPXrCwOtoYjyyn7BV29/MZfsVzpLDdRtuIZnbpXzvlf+ev8MvYr/Gqk4H/kV/G3csdazLuyTMPsbFhzd1UabQbjFvDRmcWJxR3zcfHkVw9GfpbJmeev9F08WW8uDkaslwX6avlWGU6NRKz0g/SHtCy9J30o/ca9zX3Kfc19zn3BXQKRO8ud477hLnAfc1/G9mrzGlrfexZ5GLdn6ZZrrEohI2wVHhZywjbhUWEy8icMCGNCUdiBlq3r+xafL549HQ5jH+an+1y+LlYBifuxAvRN/lVVVOlwlCkdVm9NOL5BE4wkQ2SMlDZU97hX86EilU/lUmkQUztTE6mx1EEPh7OmdqBtAvv8HdWpbrJS6tJj3n0CWdM6busNzRV3S9KTYhqvNiqWmuroiKgYhshMjmhTh9ptWhsF7970j/SbMrsPE1suR5z7DMC+P/Hs+y7ijrQAlhyAgccjbhjPygfeBTjzhNqy28EdkUh8C+DU9+z2v/oyeH791OncxHOs5y2AtTc7nb/f73TWPkD/qwBnjX8BoJ98VVBg/m8AAAEVSURBVEgN7VXBDcIwDHQqRmKATsME/Gn/TMA0HYCdis/tpY6VVqC8QFhynNi5i7kmIs1q0mBdA9agzQQndpBS4vSjCNSXa9As4p9A5Ac0yG+BD6B2r49eSfEWamASM0ayQsSxF6HL8yJwrBEBjGBNbV9h6EVudwWpMdpiHWYlgUcrOmBxvD44NTK/jiRZA3QAi6d7MOrDhHGzTMBUPIF5xHTeOmO+IMhdTIuY3BRPZR4x3wOCfRHz26RDr75aJOuQr4EBNPAKZKjtnfV3m2sR/zLLGtHNrbbuUzLcOXPToBCuIpSd7u6AF7Njaxb3wCju1IqvgJ5oKMBquaWyjJnAixOVPqrZT/AbPHttHvfmDmqb38m9APu7aLyM3S4UAAAAAElFTkSuQmCC"}}, None)
         self.assertTrue(isinstance(actual, LedBitmapObject))
 
 
