9e328ff58ee9d18c323fc2f1680ad435ff8e4ff0
code560 <smartphone.560@gmail.com>
2017-10-22 22:33:30 +0900

    文字列サイネージ
    指定の文字列を左から右へ流します。

--- libled/object/led_text_obj.py
+++ libled/object/led_text_obj.py
@@ -2,26 +2,56 @@
 from led_object import LedObject
 from ..led_cube import *
 from PIL import Image, ImageDraw, ImageFont
+from ..util.color import Color
 
 
 class LedTextObject(LedObject):
 
-    def __init__(self, x, y, z, text, color, lifetime = 0 ):
+    def __init__(self, x=LED_WIDTH-1, y=0, z=0, text="", fontsize=26, color="#ff0000", lifetime=0):
         super(LedTextObject, self).__init__(lifetime)
         self.x = x
         self.y = y
         self.z = z
         self.color = color
-        font = ImageFont.truetype("asset/font/migu-1p-regular.ttf", 20)
-        self.image = Image.new('RGB', (LED_WIDTH, LED_HEIGHT))
+
+        # font 2 text
+        font = ImageFont.truetype("asset/font/migu-1p-regular.ttf", fontsize)
+        size = font.getsize(text)
+        offset = font.getoffset(text)
+        self.image = Image.new(
+            'RGB', (max(size[0], LED_WIDTH), max(size[1], LED_HEIGHT)))
         d = ImageDraw.Draw(self.image)
-        d.text((1, 1), text, font=font, fill=(255, 0, 0))
+        d.text((offset[0], -offset[1]), text=text, font=font, fill=self.color)
+
+        # moving
+        self.set_timer(0.03)
+        self.posx = 0
+
+    def is_expired(self, offset=0):
+        return abs(self.posx) >= self.image.size[0]
+
+    def on_timer(self):
+        self.left()
 
     def draw(self, canvas):
         for x in range(LED_WIDTH):
+            px = x + abs(self.posx)
             for y in range(LED_HEIGHT):
-                r,g,b = self.image.getpixel((x,y))
-                if r == 0 and g == 0 and b == 0:
-                    continue
-                canvas.set_led(x + self.x, y + self.y, self.z, (r<<16)+(g<<8)+b)
+                for z in range(LED_DEPTH):
+                    if px < 0:
+                        continue
+                    if px >= self.image.size[0]:
+                        continue
 
+                    r, g, b = self.image.getpixel((px, y))
+                    if r == 0 and g == 0 and b == 0:
+                        continue
+                    canvas.set_led(x + self.x, y + self.y, z + self.z,
+                                   (r << 16) + (g << 8) + b)
+
+        if self.x > 0:
+            self.x -= 1
+
+    def left(self):
+        if self.x <= 0:
+            self.posx -= 1
