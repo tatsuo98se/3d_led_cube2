92ebcbf59cac22d7a2cdc05336d7bd35ab7108a6
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2018-01-08 20:53:21 +0900

    improve pformance

--- libled/object/led_bitmap_obj.py
+++ libled/object/led_bitmap_obj.py
@@ -13,18 +13,22 @@ class LedBitmapObject(LedObject):
         self.y = y
         self.z = z
         self.thick = thick
+        self.size = self.image.size
+        self.X = min(self.size[0], LED_WIDTH)
+        self.Y = min(self.size[1], LED_HEIGHT)
+        self.cache = [[0 for i in range(self.Y)] for j in range(self.X)]
+
+        for x in range(self.X):
+            for y in range(self.Y):
+                self.cache[x][y] = self.image.getpixel((x,y))
+
 
     def draw(self, canvas):
 
-        size = self.image.size
-
-        X = min(size[0], LED_WIDTH)
-        Y = min(size[1], LED_HEIGHT)
-
-        for x in range(X):
-            for y in range(Y):
+        for x in range(self.X):
+            for y in range(self.Y):
                 for z in range(self.thick):
-                    r,g,b = self.image.getpixel((x,y))
+                    r,g,b = self.cache[x][y]
                     if r == 0 and g == 0 and b == 0:
                         continue
                     canvas.set_led(x + self.x, y + self.y, self.z + z, (r<<16)+(g<<8)+b)
