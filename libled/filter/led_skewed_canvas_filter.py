2afe4c9f8193318119d2a7c210ebef77aae826bb
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2018-07-08 00:56:30 +0900

    add sound to skew filter

--- libled/filter/led_skewed_canvas_filter.py
+++ libled/filter/led_skewed_canvas_filter.py
@@ -3,6 +3,10 @@ from ..led_cube import *
 from ..util.color import Color
 import math
 import time
+import numpy as np
+from ..util.sound_player import SoundPlayer as sp
+
+T = 4
 
 class LedSkewedCanvasFilter(LedCanvasFilter):
 
@@ -11,6 +15,9 @@ class LedSkewedCanvasFilter(LedCanvasFilter):
         self.t = 0
         self.last_update = time.time()
         self.axis = axis
+        self.wav = 'asset/audio/se_swing.wav'
+        self.s = self.c = 0
+        self._pre_sign = -1
 
     def pre_draw(self):
         super(LedSkewedCanvasFilter, self).pre_draw()
@@ -18,19 +25,25 @@ class LedSkewedCanvasFilter(LedCanvasFilter):
         direction = (param['a0'] - 0.5)
         speed = param['a1'] / 5.0
         self.t += direction * speed
+        self.s = math.sin(self.t*3.14*T)
+        self.c = math.cos(self.t*3.14*T)
+
+        sign = np.sign(self.s)
+        # play sound
+        if self._pre_sign != sign:
+            sp.instance().do_play(self.wav)
+        self._pre_sign = sign
+
 
     def set_led(self, x, y, z, color):
-        T = 4
-        s = math.sin(self.t*3.14*T)
-        c = math.cos(self.t*3.14*T)
         dx = LED_WIDTH / 2.0
         dy = LED_HEIGHT / 4.0 * 3
         dz = LED_DEPTH / 2.0
         if 'y' in self.axis:
-            x, y, z = ((x - dx)*c + (z - dz)*s) + dx, y, (-(x - dx)*s + (z - dz)*c) + dz
+            x, y, z = ((x - dx)*self.c + (z - dz)*self.s) + dx, y, (-(x - dx)*self.s + (z - dz)*self.c) + dz
 
         if 'z' in self.axis:
 #            x = ((x0 - dx)*c + (z0 - dy)*s) + dx 
-            x, y, z = ((x - dx)*c + (y - dy)*s) + dx, (-(x - dx)*s + (y - dy)*c) + dy, z
+            x, y, z = ((x - dx)*self.c + (y - dy)*self.s) + dx, (-(x - dx)*self.s + (y - dy)*self.c) + dy, z
 
         self.canvas.set_led(x, y, z, color)
