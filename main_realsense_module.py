bd8b216300775fb133899e9a2ddd74ac3c5a1377
Tatsuo Fukushima <tatsuo_fukushima@icloud.com>
2018-07-19 00:23:46 +0900

    add color parameter to realsense module

--- main_realsense_module.py
+++ main_realsense_module.py
@@ -1,3 +1,4 @@
+# -*- encoding:utf8 -*-
 import time
 import numpy as np
 import cv2
@@ -27,13 +28,31 @@ try:
     dev.set_device_option(rs_option.RS_OPTION_F200_LASER_POWER, 15.0)
     while True:
 
+        # [r,g,b,深度] の形式に変換する
 
         dev.wait_for_frames()
+
+        c = dev.color
+        c = cv2.cvtColor(c, cv2.COLOR_RGB2BGR)
+
         d = dev.depth /256.0
-        d = cv2.applyColorMap(d.astype(np.uint8), cv2.COLORMAP_BONE)
+
         scale = max(32.0/640, 16.0/480)
-        scaled =  get_scled_rgb_image(d, scale, scale) 
-        frame = resize2(scaled, (16, 32), (-4,0), [[0]*4])
+
+        # カラーのサイズ変換
+        scaledc =  get_scled_rgb_image(c, scale, scale) 
+        framec = resize2(scaledc, (16, 32), (-4,0), [[0]*4])
+
+        # 深度のサイズ変換
+        d = cv2.applyColorMap(d.astype(np.uint8), cv2.COLORMAP_BONE)
+        scaledd =  get_scled_rgb_image(d, scale, scale) 
+        framed = resize2(scaledd, (16, 32), (-4,0), [[0]*4])
+
+        # 深度を最後に添付する
+        frame = np.hstack((framec.reshape(16*32,3), framed[:,:,0].reshape(16*32,1)))
+
+        frame = frame.reshape(16,32,4)
+
         send_array(socket, frame)
 except:
     logger.e("initialize realsense failed.:" + str(sys.exc_info()[0]))
