02eb0fe6ec2423c76a3c17f456fb2fa3c4b1fb5e
Tatsuo Fukushima <tatsuo.fukushima@fxat.co.jp>
2018-07-23 13:54:49 +0900

    send the sig abort to processes when script ends

--- run_all.rb
+++ run_all.rb
@@ -1,6 +1,30 @@
-pid = spawn("python ./main_hw_controller_module.py")
-#pid = spawn("python ./main_hw_controller_module_dummy.py")
-pid = spawn("python ./main_realsense_module.py")
-pid = spawn("python ./main_paint_service.py")
-pid = spawn("python ./main_realsense_service.py")
-pid = spawn("python ./main_block_service.py")
+require "timeout"
+
+begin
+pids = [
+spawn("python ./main_hw_controller_module.py"),
+#spawn("python ./main_hw_controller_module_dummy.py"),
+spawn("python ./main_realsense_module.py"),
+spawn("python ./main_paint_service.py"),
+spawn("python ./main_realsense_service.py"),
+spawn("python ./main_block_service.py"),
+]
+end
+
+begin
+    sleep
+rescue SignalException
+    pids.each do |pid|
+        begin
+            Timeout.timeout(5) do
+                puts "[INFO] aborting PID:#{pid}. waiting..."
+                Process.kill("INT", pid)
+                Process.wait pid
+                puts "[INFO] PID:#{pid} has aborted successfully."
+            end
+        rescue Timeout::Error
+            puts "[ERR] PID:#{pid} has not aborted. the process will be killed forcibly."
+            Process.kill(:KILL, pid)
+        end
+    end
+end
